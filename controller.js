4ab32507c36f40208e861ebeb8645a0b2d89b8e1
Deven Olson <olsondeven@gmail.com>
2016-10-25 13:38:52 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,5 +1,11 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles').controller('mainController', function($scope, mainService){
+  //this is to test angular
+  $scope.thisAppIsBroken = "This angular app is working";
+  //inject the mainService in the controller
+  $scope.getUsers = function(){
+    $scope.users = mainService.getUsers();
+  };
+  //this is storing the data in a var users
+  $scope.getUsers();
+  $scope.toggleFavorite = mainService.toggleFavorite;
+});
